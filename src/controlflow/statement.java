dcb4d5ea37d956ce713ab8bfd2f80c50dce5c533
VIJAYATHITHAN SENTHILKUMAR <srikeerthana098@gmail.com>
2025-06-10 11:29:02 +0530

    add file

--- src/controlflow/statement.java
+++ src/controlflow/statement.java
@@ -2,15 +2,18 @@ package controlflow;
 
 public class statement {
     public void Operator(int number){
-        if(number%2==0){
+        if(number%2 == 0)
+        {
             System.out.println("Even number");
-        }else {
+        }
+        else
+        {
             System.out.println("Odd number");
         }
     }
 
     public static void main(String[] args) {
-     statement statement=new statement();
-     statement.Operator(5);
+      statement statement=new statement();
+      statement.Operator(5);
     }
 }
