231a79abbdcd083dfc968f6968e42acb55c77a07
VIJAYATHITHAN SENTHILKUMAR <srikeerthana098@gmail.com>
2025-06-10 22:17:51 +0530

    add file

--- src/com/myjava/array/Multidimensional.java
+++ src/com/myjava/array/Multidimensional.java
@@ -6,16 +6,15 @@ public class Multidimensional {
     public static void main(String[] args) {
 
             Scanner sc = new Scanner(System.in);
-            int n=sc.nextInt();
-            String[][] n1 = new String[n][n];
 
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
+            String[][] n1 = new String[6][5];
+            for(int i=0;i<6;i++){
+                for(int j=0;j<5;j++){
                     n1[i][j]= sc.next();
                 }
             }
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n1.length; j++) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 6; j++) {
                     System.out.println(n1[j][i]);
                 }
             }
