b5a47dee34f64a7b323a99c69ef1a212bd31302c
VIJAYATHITHAN SENTHILKUMAR <srikeerthana098@gmail.com>
2025-06-10 23:11:44 +0530

    add file

--- src/com/myjava/array/MultidimensionalEven.java
+++ src/com/myjava/array/MultidimensionalEven.java
@@ -7,17 +7,16 @@ public class MultidimensionalEven {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int[][] n1 = new int[5][5];
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                n1[i][j]= sc.nextInt();
+            for(int i=0;i<5;i++){
+                for(int j=0;j<5;j++){
+                    n1[i][j]= sc.nextInt();
+                }
             }
-        }
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                int a=n1[j][i];
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    int a=n1[j][i];
                     if(a%2==0){
                         System.out.println(a);
                     }
@@ -25,6 +24,8 @@ public class MultidimensionalEven {
 
 
             }
+
+
         }
     }
 
--- src/com/myjava/array/sensortemp1.java
+++ src/com/myjava/array/sensortemp1.java
@@ -7,20 +7,16 @@ public class sensortemp1 {
 
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();//6 row
-        int n2=sc.nextInt();//1 col
-        int [][] n1 = new int[n][n2];
+        int [] n1 = new int[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n2; j++) {
-                n1[i][j] = sc.nextInt();
+                n1[i]= sc.nextInt();
             }
-        }
-        for (int i = 0; i < n2; i++) {
-            for (int j = 0; j < n; j++) {
-                int a=n1[j][i];
-                if(a<18 || a>28){
-                    System.out.println(a + "out is range");
+
+            for (int i = 0; i < n; i++) {
+                if(n1[i]<18 || n1[i]>28){
+                    System.out.println(n1[i] + "out is range");
                 }
             }
         }
     }
-}
+
