70e5e9ae29e1f01438e8bacf0d63bcc0c7606674
VIJAYATHITHAN SENTHILKUMAR <srikeerthana098@gmail.com>
2025-06-16 14:53:53 +0530

    add file

--- src/com/myjava/String/NameDegree.java
+++ src/com/myjava/String/NameDegree.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class NameDegree {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name= sc.next();
-        String degree= sc.next();
-        System.out.println(name+" "+degree);
+        String name= sc.nextLine();
+        String degree= sc.nextLine();
+        String college= sc.nextLine();
+        StringBuilder namewithdegree = new StringBuilder();
+        namewithdegree.append(name);
+        namewithdegree.append(degree);
+        namewithdegree.append(college);
+        System.out.println("I'm " + name + " I have completed my " + degree+ " in college of "+college);
+
     }
 }
