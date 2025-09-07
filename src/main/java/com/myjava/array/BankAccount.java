9adab411101bc0700694a3e36883527e90e59176
Keerthanasri-S <srikeerthana098@gmail.com>
2025-09-07 21:36:30 +0530

    files added

--- src/main/java/com/myjava/array/BankAccount.java
+++ src/main/java/com/myjava/array/BankAccount.java
@@ -3,7 +3,18 @@ package com.myjava.array;
 import java.util.Scanner;
 
 public class BankAccount {
-
+    private int balance;
+    BankAccount(int balance){
+        this.balance= balance;
+    }
+    public void deposit(int amount){
+        balance+=amount;
+        System.out.println(balance);
+    }
+    public void  credit(int amount){
+        balance-=amount;
+        System.out.println(balance);
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
