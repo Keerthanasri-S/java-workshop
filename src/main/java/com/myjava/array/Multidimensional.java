74cc1ba3c771978514f0d6ef89b7b3ea196ac1eb
Keerthanasri-S <srikeerthana098@gmail.com>
2025-09-08 22:05:03 +0530

    files added

--- src/main/java/com/myjava/array/Multidimensional.java
+++ src/main/java/com/myjava/array/Multidimensional.java
@@ -7,20 +7,7 @@ public class Multidimensional {
 
             Scanner sc = new Scanner(System.in);
 
-            String[][] TemperatureArray = new String[5][6];
-            for(int i=0;i<5;i++){
-                for(int j=0;j<6;j++){
-                    TemperatureArray[i][j]= sc.next();
-                }
-            }
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 6; j++) {
-                    System.out.print(TemperatureArray[i][j]+" ");
-                    if(j==5){
-                        System.out.print(",");
-                    }
 
-                }
                 System.out.println();
             }
         }
