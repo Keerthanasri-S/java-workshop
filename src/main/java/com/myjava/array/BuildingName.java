77c718aa0c563fa59b90cda4dd60848291cf3727
Keerthanasri-S <srikeerthana098@gmail.com>
2025-09-20 22:13:12 +0530

    files added

--- src/main/java/com/myjava/array/BuildingName.java
+++ src/main/java/com/myjava/array/BuildingName.java
@@ -6,7 +6,14 @@ public class BuildingName {
     public void build(){
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
-
+        String[] building=new String[n];
+        for(int i=0;i<n;i++) {
+            building[i]=sc.next();
+        }
+        for(int i=0;i<n;i++) {
+            System.out.println(building[i]);
+        }
+    }
     public static void main(String[] args) {
         BuildingName buildingName=new BuildingName();
         buildingName.build();
